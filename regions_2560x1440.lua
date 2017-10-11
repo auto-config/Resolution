b52c9d80c134cdbe0fdf5558fc6e03a270c32f2d
auto-config <32314328+auto-config@users.noreply.github.com>
2017-10-11 16:57:17 -0500

    Add files via upload

---
+++ regions_2560x1440.lua
@@ -0,0 +1,177 @@
+--screen = getRealScreenSize()
+
+--ISLAND/MAP NAVIGATION:
+closeXScenarioRegion = Region(2650, 0, 130, 130)
+battleIconRegion = Region(1540, 1190, 230, 220)
+moveIconRegion = Region(2550, 1190, 230, 220)
+--connectionDelayRegion = Region(530, 330, 870, 195)
+--connectionYesRegion = Region(740, 685, 95, 65)
+backButtonRegion = Region(2540, 1190, 240, 240)
+--dontShowRegion = Region(205, 950, 295, 85)
+touchStartRegion = Region(1300, 560, 160, 740)
+monthlyEventXRegion = Region(2130, 50, 120, 100)
+mailboxXRegion = Region(2230, 160, 100, 110)
+
+
+--FODDER:
+FindEmptyFodderSlotsRegion = Region(2160, 270, 50, 60)
+FindFillFodderSlotsRegion = Region(2100, 270, 50, 60)
+fodderStorageOkRegion = Region(1330, 1220, 130, 80)
+storageIconRegion = Region(50, 880, 220, 220)
+
+
+--RUNES:
+mainStatRegion = Region(1150, 440, 450, 110)
+runeSlotRegion = Region(900, 340, 900, 90)
+runeRarityRegion = Region(1670, 460, 220, 90)
+runeRankRegion = Region(970, 460, 160, 40)
+runeRegion = Region(970, 460, 160, 160)
+runeYesRegion = Region(1000, 790, 340, 150)
+sellRegion = Region(1000, 1080, 340, 150)
+getRegion = Region(1170, 700, 710, 560)
+runeSubRegion = Region(930, 640, 640, 320)
+--runeStatRegion = Region(0, 200, 700, 250) (SCALE ME)
+
+
+--BATTLE PREP:
+startRegion = Region(2180, 1020, 180, 80)
+startDialogRegion = Region(0, 0, 800, 110)
+noLeaderSkillRegion = Region(1600, 470, 250, 110)
+noLeaderSkillYesRegion = Region(1000, 800, 340, 140)
+closeXStartDialog = Region(2640, 0, 130, 130)
+battleSlot1Region = Region(590, 270, 210, 210)
+battleSlot2Region = Region(320, 400, 210, 210)
+battleSlot3Region = Region(840, 400, 210, 210)
+battleSlot4Region = Region(590, 550, 210, 210)
+friend1Location = Location(140, 1250)
+
+
+--VICTORY/DEFEAT:
+victoryDiamondRegion = Region(2140, 640, 150, 180)
+--replayRegion = Region(490, 550, 170, 75)
+--nextRegion = Region(1145, 555, 250, 65)
+--reviveNoRegion = Region(1190, 675, 95, 60)
+--refillYesRegion = Region(735, 625, 105, 55)
+--refillNoRegion = Region(1050, 625, 120, 60)
+--notEnoughEnergyRegion = Region(955, 355, 225, 65)
+--notEnoughWingRegion = Region(850, 355, 260, 65)
+--rechargeFlashRegion = Region(455, 395, 195, 235)
+--slot1MaxRegion = Region(450,668,345,165)
+--slot2MaxRegion = Region(825,668,345,165)
+--slot3MaxRegion = Region(1215,668,345,165)
+--slot4MaxRegion = Region(435,797,345,165)
+
+
+--ARENA:
+--victoryDefeatStageRegion = Region(670, 235, 605, 65)
+--victoryDefeatRegion = Region(860, 105, 230, 145)
+--arenaWingRegion = Region(1455, 200, 200, 750)
+--arenaRankRegion = Region(910, 115, 400, 400)
+--arenaMatchupRegion = Region(200, 200, 250, 100)
+--arenaRivalRegion = Region(200, 320, 250, 100)
+--arenaRivalNumberRegion = Region(175, 325, 300, 150)
+--arenaRivalDialogRegion = Region(0, 775, 125, 125)
+--arenaBottomRegion = Region(985, 810, 230, 60)
+--arenaOkRegion = Region(920, 625, 85, 55)
+--arenaXRegion = Region(1600, 100, 200, 200)
+--arenaFailLoadRegion = Region(575, 355, 255, 55)
+--refreshListRegion = Region(1300, 200, 400, 400)
+--refreshList2Region = Region(845, 560, 155, 55)
+--refreshListNotReadyRegion = Region(925, 555, 110, 50)
+
+
+--DUNGEON:
+closeXCairoDungeonRegion = Region(2280, 100, 120, 120)
+dungeonListRegion = Region(420, 370, 750, 1010)
+dungeonBattleRegion = Region(1470, 370, 200, 1030)
+dropInfoRegion = Region(1910, 260, 400, 70)
+
+
+--ISLAND ITEMS:
+closeXMagicShopRegion = Region(2620, 60, 130, 120)
+
+
+--TOA:
+closeXToaRegion = Region(2280, 90, 110, 110)
+--toaNextStageRegion = Region(320, 510, 585, 145)
+
+
+--BATTLE:
+playRegion = Region(390, 1270, 130, 140)
+pauseRegion = Region(1190, 570, 390, 120)
+
+
+--MAGIC SHOP:
+magicShopItemRegion = Region(1780, 330, 280, 880)
+magicShopTimeRegion = Region(1970, 260, 90, 70)
+
+
+--WHAT IS?
+--okRegion = Region(900, 730, 120, 300)
+--dialogTextCenterRegion = Region(600, 0, 750, 275)
+--dialogToaRegion = Region(710, 65, 305, 85)
+--yesPurchaseRegion = Region(745, 635, 110, 60)
+--okPurchaseRegion = Region(915, 625, 90, 60)
+--closePurchaseRegion = Region(900, 895, 125, 60)
+--closeXPurchaseRegion = Region(1600, 0, 320, 320)
+--yesRegion = Region(740, 600, 125, 300)
+--fodderCommandRegion = Region(640, 550, 600, 50)
+--commandRegion = Region(660, 450, 600, 50)
+--resultRegion = Region(1580, 200, 700, 250)
+--dialogCairoDungeonRegion = Region(715, 70, 485, 60)
+--sdChargeRegion = Region(1450, 310, 185, 140)
+--samsungSearchRegion = Region(150, 75, 215, 50)
+--closeXRegion = Region(1350, 0, 570, 350)
+--iconPurchaseRegion = Region(1360, 895, 180, 170)
+--dialogScenarioRegion = Region(950, 0, 1000, 200)
+--closeDialogScenarioRegion = Region(1650, 0, 300, 200)
+--closeNowYesRegion = Region(600, 630, 350, 200)
+--closeNowRegion = Region(525, 420, 75, 80)
+--runeSellTextRegion = Region(535, 360, 865, 130)
+
+
+
+
+
+--VICTORY ITEMS:
+--grindstoneRegion = Region(760, 450, 300, 100)
+--enchantedGemRegion = Region(750, 350, 350, 150)
+
+
+--RAID:
+--raidJoinRegion = Region(1300, 845, 250, 65)
+--raidReadyRegion = Region(1550, 950, 250, 75)
+--okRaidRegion = Region(910, 615, 100, 80)
+--raidVictoryTotalRegion = Region(125, 600, 150, 50)
+--raidLossTotalRegion = Region(125, 725, 150, 50)
+--raidOkRegion = Region(825, 625, 250, 250)
+--sameSessionRegion = Region(780, 360, 180, 50)
+--raidGetRegion = Region(1035, 635, 145, 115)
+
+
+--LIVE ARENA:
+--slot1Region = Region(0, 675, 200, 225)
+--slot2Region = Region(0, 825, 200, 225)
+--slot3Region = Region(190, 675, 200, 225)
+--slot4Region = Region(190, 825, 200, 225)
+--slot5Region = Region(300, 675, 200, 225)
+--slot6Region = Region(300, 825, 200, 225)
+--slot7Region = Region(470, 675, 200, 225)
+--slot8Region = Region(470, 825, 200, 225)
+--slot9Region = Region(620, 675, 200, 225)
+--slot10Region = Region(620, 825, 200, 225)
+--liveArenaMapRegion = Region(0, 750, 250, 500)
+--liveArenaTopRegion = Region(350, 0, 1100, 200)
+--liveArenaBattleRegion = Region(1450, 420, 420, 125)
+--liveArenaLookingRegion = Region(550, 400, 900, 200)
+--liveArenaDialogRegion = Region(600, 0, 800, 150)
+--liveArenaSameMonOkRegion = Region(750, 500, 450, 200)
+
+
+--RIFT:
+--riftBattleRegion = Region(1345, 850, 155, 65)
+--riftRankRegion = Region(1440, 740, 225, 90)
+--riftYesRegion = Region(650, 585, 285, 140)
+--riftStrategyRegion = Region(290, 155, 270, 75)
+--riftBackRegion = Region(1735, 900, 175, 160)
+--closeXRiftRegion = Region(1635, 35, 70, 70)
